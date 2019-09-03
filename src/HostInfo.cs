9269068f522a31ac1023ed0455036de32d24bc16
igece <igcelis@gmail.com>
2019-09-03 18:46:51 +0200

    Host information API method (shodan/host/{ip}) implemented.

---
+++ src/HostInfo.cs
@@ -0,0 +1,36 @@
+﻿using Shodan.API.Exceptions;
+using Shodan.API.JsonTypes;
+
+
+namespace Shodan.API
+{
+  public class HostInfo : ShodanMethod<HostJson>
+  {
+    public string IP { get; set; }
+
+    public bool? Minify { get; set; }
+
+    
+    public HostInfo()
+    : base("shodan/host/")
+    {
+      IP = string.Empty;
+      Minify = null;
+    }
+
+
+    public override HostJson Execute()
+    {
+      if (string.IsNullOrEmpty(IP))
+        throw new ShodanException("No IP specified.");
+
+      MethodUrl = $"shodan/host/{IP}";
+      RequestParams.Clear();
+
+      if (Minify.HasValue)
+        RequestParams.Add("minify", Minify.Value.ToString().ToLowerInvariant());
+
+      return base.Execute();
+    }
+  }
+}
