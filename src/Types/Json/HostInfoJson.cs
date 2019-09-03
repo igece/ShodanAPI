3354cf73af1a0cd3d365d809265e1894d6f191ba
igece <igcelis@gmail.com>
2019-09-03 19:10:49 +0200

    More properties added.

--- src/Types/Json/HostInfoJson.cs
+++ src/Types/Json/HostInfoJson.cs
@@ -8,6 +8,33 @@ namespace Shodan.API.Types.Json
   [DataContract]
   public class HostInfoJson
   {
+    [DataMember(Name = "region_code")]
+    public string RegionCode { get; set; }
+
+    [DataMember(Name = "country_code")]
+    public string CountryCode { get; set; }
+
+    [DataMember(Name = "dma_code")]
+    public string DmaCode { get; set; }
+
+    [DataMember(Name = "city")]
+    public string City { get; set; }
+
+    [DataMember(Name = "country_code3")]
+    public string CountryCode3 { get; set; }
+
+    [DataMember(Name = "country_name")]
+    public string CountryName { get; set; }
+
+    [DataMember(Name = "ip")]
+    public uint IP { get; set; }
+
+    [DataMember(Name = "ip_str")]
+    public string IPString { get; set; }
+
+    [DataMember(Name = "isp")]
+    public string ISP { get; set; }
+
     [DataMember(Name = "data")]
     public HostJson[] Data { get; set; }
   }
