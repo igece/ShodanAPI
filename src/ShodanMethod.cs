bd27dcc88ee7f3251d54707516f6ec8fe66ffd79
igece <igcelis@gmail.com>
2019-09-03 18:44:22 +0200

    Allow classes specializing ShodanMethod to modify the method url.

--- src/ShodanMethod.cs
+++ src/ShodanMethod.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using System.Net;
 using System.Runtime.Serialization.Json;
+
 using Shodan.API.Exceptions;
+
 
 namespace Shodan.API
 {
   public class ShodanMethod<T> where T : class
   {
-    protected string MethodUrl { get; private set; }
+    protected string MethodUrl { get; set; }
 
     protected Dictionary<string, string> RequestParams { get; private set; }
 
@@ -28,9 +30,9 @@ namespace Shodan.API
       string requestParamStr = String.Empty;
 
       foreach (KeyValuePair<string, string> requestParam in RequestParams)
-        requestParamStr += String.Format("&{0}={1}", requestParam.Key, requestParam.Value);
+        requestParamStr += $"&{requestParam.Key}={requestParam.Value}";
 
-      HttpWebRequest request = WebRequest.Create(String.Format("{0}/{1}?key={2}{3}", ShodanApi.URL, MethodUrl, ShodanApi.Key, requestParamStr)) as HttpWebRequest;
+      HttpWebRequest request = WebRequest.Create($"{ShodanApi.URL}/{MethodUrl}?key={ShodanApi.Key}{requestParamStr}") as HttpWebRequest;
       DataContractJsonSerializer jsonSerializer;
       object objResponse;
 
