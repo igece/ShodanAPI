659cffb1a56d20f80dc9636315f0f097d369688a
Israel Gómez de Celis González <igcelis@gmail.com>
2022-02-14 11:42:06 +0100

    Fixed: Check that HTTP request error effectively contains a Shodan error message

--- src/ShodanClient.cs
+++ src/ShodanClient.cs
@@ -13,104 +13,115 @@ using Shodan.API.Types.Responses;
 
 namespace Shodan.API
 {
-  public partial class ShodanClient
-  {
-    public static string ApiKey { get; set; }
-
-    public static string Url { get; set; } = "https://api.shodan.io";
-
-    public static uint ResultsPerPage { get; set; } = 100;
-
-
-    private static StringBuilder GetRequestBuilder(string methodPath)
+    public partial class ShodanClient
     {
-      if (string.IsNullOrEmpty(ApiKey))
-        throw new ShodanException("Shodan API key not specified");
+        public static string ApiKey { get; set; }
 
-      return new StringBuilder($"{Url}{methodPath}?key={ApiKey}");
-    }
+        public static string Url { get; set; } = "https://api.shodan.io";
+
+        public static uint ResultsPerPage { get; set; } = 100;
 
 
-    private static async Task<T> MakeRequest<T>(string request)
-    {
-      var httpClient = new HttpClient();
-      HttpResponseMessage response = null;
-      bool timeout;
-      var retries = 0;
-
-      try
-      {
-        do
+        private static StringBuilder GetRequestBuilder(string methodPath)
         {
-          timeout = false;
-          response = await httpClient.GetAsync(request);
+            if (string.IsNullOrEmpty(ApiKey))
+                throw new ShodanException("Shodan API key not specified");
 
-          if (response.IsSuccessStatusCode)
-            return await response.Content.ReadFromJsonAsync<T>();
-
-          if (response.StatusCode == HttpStatusCode.RequestTimeout && retries++ < 5)
-          {
-            System.Diagnostics.Debug.WriteLine($"Request timed out, retrying... ({retries}/5)");
-            timeout = true;
-
-            await Task.Delay(1000);
-            continue;
-          }
-
-          var shodanError = await response.Content.ReadFromJsonAsync<Error>();
-
-          if (shodanError != null)
-            throw new ShodanException(shodanError.Message);
-        } while (timeout);
-
-        throw new HttpRequestException($"{response.StatusCode} {response.ReasonPhrase}");
-      }
-
-      finally
-      {
-        response?.Dispose();
-      }
-    }
-
-
-    private static async Task<T> MakeMultipageRequest<T, V>(string request, uint maxPages) where T : IPagedResults<V>, new() where V : class, new()
-    {
-      if (maxPages == 1)
-        return await MakeRequest<T>(request);
-
-      uint currentPage = 1;
-      uint realMaxPages = maxPages;
-
-      var allResults = new T();
-      var allItems = new List<V>();
-
-      do
-      {
-        try
-        {
-          var pageResults = await MakeRequest<T>($"{request}&page={currentPage}");
-
-          if (currentPage == 1)
-          {
-            allResults.Total = pageResults.Total;
-            uint totalPages = Convert.ToUInt32(Math.Ceiling(pageResults.Total / Convert.ToDouble(ResultsPerPage)));
-
-            if (realMaxPages > totalPages)
-              realMaxPages = totalPages;
-          }
-
-          allItems.AddRange(pageResults.Matches);
-          await Task.Delay(1000);
+            return new StringBuilder($"{Url}{methodPath}?key={ApiKey}");
         }
 
-        catch (Exception)
+
+        private static async Task<T> MakeRequest<T>(string request)
         {
+            var httpClient = new HttpClient();
+            HttpResponseMessage response = null;
+            bool timeout;
+            var retries = 0;
+
+            try
+            {
+                do
+                {
+                    timeout = false;
+                    response = await httpClient.GetAsync(request);
+
+                    if (response.IsSuccessStatusCode)
+                        return await response.Content.ReadFromJsonAsync<T>();
+
+                    if (response.StatusCode == HttpStatusCode.RequestTimeout && retries++ < 5)
+                    {
+                        System.Diagnostics.Debug.WriteLine($"Request timed out, retrying... ({retries}/5)");
+                        timeout = true;
+
+                        await Task.Delay(1000);
+                        continue;
+                    }
+
+                    try
+                    {
+                        var shodanError = await response.Content.ReadFromJsonAsync<Error>();
+
+                        if (shodanError != null)
+                            throw new ShodanException(shodanError.Message);
+                    }
+
+                    catch (Exception)
+                    {
+                        throw new HttpRequestException($"{(int)response.StatusCode} {response.ReasonPhrase}");
+                    }
+
+                } while (timeout);
+
+                throw new HttpRequestException($"{response.StatusCode} {response.ReasonPhrase}");
+            }
+
+            finally
+            {
+                response?.Dispose();
+            }
         }
-      } while (currentPage++ < realMaxPages);
 
-      allResults.Matches = allItems.ToArray();
 
-      return allResults;
+        private static async Task<T> MakeMultipageRequest<T, V>(string request, uint maxPages)
+          where T : IPagedResults<V>, new()
+          where V : class, new()
+        {
+            if (maxPages == 1)
+                return await MakeRequest<T>(request);
+
+            uint currentPage = 1;
+            uint realMaxPages = maxPages;
+
+            var allResults = new T();
+            var allItems = new List<V>();
+
+            do
+            {
+                try
+                {
+                    var pageResults = await MakeRequest<T>($"{request}&page={currentPage}");
+
+                    if (currentPage == 1)
+                    {
+                        allResults.Total = pageResults.Total;
+                        uint totalPages = Convert.ToUInt32(Math.Ceiling(pageResults.Total / Convert.ToDouble(ResultsPerPage)));
+
+                        if (realMaxPages > totalPages)
+                            realMaxPages = totalPages;
+                    }
+
+                    allItems.AddRange(pageResults.Matches);
+                    await Task.Delay(1000);
+                }
+
+                catch (Exception)
+                {
+                }
+            } while (currentPage++ < realMaxPages);
+
+            allResults.Matches = allItems.ToArray();
+
+            return allResults;
+        }
     }
-  }
 }
